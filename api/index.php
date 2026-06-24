337d8d2107a47cb600b8778c445428701e2eae49
rahmatadisaputra2601-beep <rahmatadisaputra2601@gmail.com>
2026-06-24 13:12:21 +0700

    Uji coba gerbang polosan anti cache view

--- api/index.php
+++ api/index.php
@@ -1,22 +1,2 @@
 <?php
-
-// 1. Muat Autoload Vendor
-require __DIR__ . '/../vendor/autoload.php';
-
-// 2. Hidupkan Aplikasi Laravel
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-
-// 3. Bangun HTTP Kernel
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-// 4. Tangkap HTTP Request
-$request = Illuminate\Http\Request::capture();
-
-// 5. Eksekusi Request Lewat Kernel (Ini otomatis men-handle bootstrap luar dalam)
-$response = $kernel->handle($request);
-
-// 6. Kirim Response ke Browser
-$response->send();
-
-// 7. Matikan Proses Kernel
-$kernel->terminate($request, $response);
+echo "<h1>🚀 VERCEL SUDAH WARAS, BRO! KODE DIREKSRI SUDAH BENAR!</h1>";
